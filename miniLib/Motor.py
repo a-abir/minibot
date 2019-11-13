dd4e7aa1cd37384503e3ca6f9533d977495adbcd
atikul99 <atikul10152002@gmail.com>
2019-11-12 22:06:24 -0500

    Update the sample code file to code block

--- miniLib/Motor.py
+++ miniLib/Motor.py
@@ -1,8 +1,20 @@
-from setup import Motors
+from parse import Motors
 
 class Motor:
     def __init__(self, ID):
+        '''
+        Inintialize the DC Motor
+
+        :param ID: The ID of the Motor [0,1]
+        :type ID: int
+        '''
         self.motor = Motors[ID]
 
     def throttle(self, power):
+        '''
+        Input power for the Motor
+
+        :param power: Value from -1 to 1
+        :type power: float
+        '''
         self.motor.throttle = power
--- miniLib/Servo.py
+++ miniLib/Servo.py
@@ -1,16 +1,40 @@
-from setup import Servos, ContiniousServos
+from parse import Servos, ContiniousServos
 
 class Servo:
     def __init__(self, ID):
+        '''
+        Initialize a Servo
+
+        :param ID: ID of the Servo [0,1,2,3]
+        :type ID: int
+        '''
         self.servo = Servos[ID]
 
     def angle(self, degree):
+        '''
+        Set the angle to rotate to
+
+        :param degree: degree of the Servo
+        :type degree: int
+        '''
         self.servo.angle = degree
 
 
 class ContiniousServo:
     def __init__(self, ID):
+        '''
+        Initialize a Continious Servo
+
+        :param ID: ID of the Continious Servo [0,1,2,3]
+        :type ID: int
+        '''
         self.servo = ContiniousServos[ID]
 
     def throttle(self, power):
+        '''
+        Set the throttle of the Continious Servo
+
+        :param power: Power of the Continious Servo -1 to 1
+        :type power: float
+        '''
         self.servo.throttle = power
